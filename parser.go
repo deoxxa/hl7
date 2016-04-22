53bdbd6a6abf7aca2955c3ce31e6f4b3c23221b0
deoxxa <deoxxa@fknsrs.biz>
2016-04-22 20:33:35 +1000

    add Segment() and Segments() lookup functions to Message

--- parser.go
+++ parser.go
@@ -22,6 +22,33 @@ type (
 	Subcomponent string
 )
 
+func (m Message) Segments(name string) []Segment {
+	var a []Segment
+
+	for _, s := range m {
+		if string(s[0][0][0][0]) == name {
+			a = append(a, s)
+		}
+	}
+
+	return a
+}
+
+func (m Message) Segment(name string, index int) Segment {
+	i := 0
+	for _, s := range m {
+		if string(s[0][0][0][0]) == name {
+			if i == index {
+				return s
+			}
+
+			i++
+		}
+	}
+
+	return nil
+}
+
 type Delimiters struct {
 	Field, Component, Repeat, Escape, Subcomponent byte
 }
