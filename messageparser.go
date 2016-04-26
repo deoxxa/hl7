aafc6d6732f9058e61db07f27c4fd04deeff8741
deoxxa <deoxxa@fknsrs.biz>
2016-04-26 20:25:15 +1000

    annotate message parser

--- messageparser.go
+++ messageparser.go
@@ -18,14 +18,29 @@ type Delimiters struct {
 	Field, Component, Repeat, Escape, Subcomponent byte
 }
 
+// ParseMessage takes input as a `[]byte`, and returns the whole message, the
+// control characters (as `*Delimiters`), and maybe an error.
 func ParseMessage(buf []byte) (Message, *Delimiters, error) {
+	// This is a sanity check, to make sure the message is long enough to
+	// contain a valid header. If it's less than eight bytes long, it can't
+	// possibly contain the required information.
+
 	if len(buf) < 8 {
 		return nil, nil, ErrTooShort(stackerr.Newf("message must be at least eight bytes long; instead was %d", len(buf)))
 	}
 
+	// Every valid HL7 message will begin with `MSH`. This isn't specifically
+	// mandated in the specification, but by combining a few constraints, we can
+	// safely come to this conclusion. This allows us to reject junk data pretty
+	// quickly.
+
 	if !bytes.HasPrefix(buf, []byte("MSH")) {
 		return nil, nil, ErrInvalidHeader(stackerr.Newf("expected message to begin with MSH; instead found %q", buf[0:3]))
 	}
+
+	// These are the control characters. `fs` is the field separator, `cs` the
+	// component separator, `rs` the field repeat separator, `ec` the escape
+	// character, and `ss` the sub-component separator.
 
 	fs := buf[3]
 	cs := buf[4]
@@ -34,6 +49,11 @@ func ParseMessage(buf []byte) (Message, *Delimiters, error) {
 	ss := buf[7]
 
 	d := Delimiters{fs, cs, rs, ec, ss}
+
+	// These are the variables we'll be working with. We reuse these variables a
+	// lot in the parsing loop below. A `FieldItem` is one instance of a field
+	// value - the HL7 standard calls this a "repetition," but I found that
+	// `FieldItem` was easier to think about.
 
 	var (
 		message   Message
@@ -44,11 +64,25 @@ func ParseMessage(buf []byte) (Message, *Delimiters, error) {
 		s         []byte
 	)
 
+	// We manually construct the first few fields of the message, as we know
+	// that it has to be structured this way. It's easier than having special
+	// code to parse these weird fields out.
+
 	segment = Segment{
 		Field{FieldItem{Component{Subcomponent("MSH")}}},
 		Field{FieldItem{Component{Subcomponent(buf[3])}}},
 		Field{FieldItem{Component{Subcomponent(string(buf[4:8]))}}},
 	}
+
+	// These functions are used when we encounter control characters. When we
+	// see a control character, it signals the end of a certain kind of element.
+	// `|` means the end of a field, `~` a repetition, `^` a component, and `&`
+	// a subcomponent. Another property of these separators is that each one not
+	// only ends that element itself, but also any elements it contains. For
+	// example, hitting `|` not only means that you've found the end of the
+	// current field, but also the end of the current repetition, component, and
+	// sub-component. This is expressed below as nested calls in the different
+	// `commitX` functions.
 
 	commitBuffer := func(force bool) {
 		if s != nil || force {
@@ -93,6 +127,12 @@ func ParseMessage(buf []byte) (Message, *Delimiters, error) {
 		}
 	}
 
+	// This is the main parse loop. We go through the buffer byte-by-byte,
+	// accumulating data until we hit any of the control characters. When we do,
+	// we commit whatever we have "buffered" for that level. Carriage returns
+	// and line breaks count as control characters, as they delimit segments
+	// themselves.
+
 	sawNewline := false
 	for _, c := range buf[9:] {
 		switch c {
@@ -119,7 +159,13 @@ func ParseMessage(buf []byte) (Message, *Delimiters, error) {
 		}
 	}
 
+	// After we've gotten to the end of the input, we might still have some data
+	// buffered up, so we make sure that gets committed.
+
 	commitSegment(false)
+
+	// That's it - we're done! Return the message, the `Delimiters` object, and
+	// `nil` - signalling that there was no error.
 
 	return message, &d, nil
 }
