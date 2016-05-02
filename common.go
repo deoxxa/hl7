3c723d7aaee991db0fd81ac45ba0bd2383bf5b9f
deoxxa <deoxxa@fknsrs.biz>
2016-05-02 10:11:24 +1000

    add canonical import comment

--- common.go
+++ common.go
@@ -1,4 +1,4 @@
-package hl7
+package hl7 // import "fknsrs.biz/p/hl7"
 
 func max(a, b int) int {
 	if a > b {
--- message.go
+++ message.go
@@ -1,4 +1,4 @@
-package hl7
+package hl7 // import "fknsrs.biz/p/hl7"
 
 type (
 	Message      []Segment
